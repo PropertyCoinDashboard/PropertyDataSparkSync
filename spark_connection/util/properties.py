5ba112cc055525d301d3415e2181975212c40e17
sky81219 <=>
2023-09-11 15:34:20 +0900

    refact: 고도화-시작

---
+++ spark_connection/util/properties.py
@@ -0,0 +1,28 @@
+import configparser
+from pathlib import Path
+
+path = Path(__file__).parent.parent
+parser = configparser.ConfigParser()
+parser.read(f"{path}/util/urls.conf")
+
+BTC_TOPIC_NAME: str = parser.get("TOPICNAME", "BTC_TOPIC_NAME")
+ETH_TOPIC_NAME: str = parser.get("TOPICNAME", "ETHER_TOPIC_NAME")
+OTHER_TOPIC_NAME: str = parser.get("TOPICNAME", "OTHER_TOPIC_NAME")
+
+BTC_AVERAGE_TOPIC_NAME: str = parser.get("AVERAGETOPICNAME", "BTC_AVERAGE_TOPIC_NAME")
+ETH_AVERAGE_TOPIC_NAME: str = parser.get("AVERAGETOPICNAME", "ETHER_AVERAGE_TOPIC_NAME")
+
+
+BTC_TOPIC_NAME: str = parser.get("TOPICNAME", "BTC_TOPIC_NAME")
+ETH_TOPIC_NAME: str = parser.get("TOPICNAME", "ETHER_TOPIC_NAME")
+
+
+UPBIT_BTC_REAL_TOPIC_NAME: str = parser.get(
+    "REALTIMETOPICNAME", "UPBIT_BTC_REAL_TOPIC_NAME"
+)
+BITHUMB_BTC_REAL_TOPIC_NAME: str = parser.get(
+    "REALTIMETOPICNAME", "BITHUMB_BTC_REAL_TOPIC_NAME"
+)
+KORBIT_BTC_REAL_TOPIC_NAME: str = parser.get(
+    "REALTIMETOPICNAME", "KORBIT_BTC_REAL_TOPIC_NAME"
+)
