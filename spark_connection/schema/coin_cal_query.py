52ce812a13923f0bd439d4b4cdc8a09cab881ef2
sky81219 <=>
2024-05-28 22:19:29 +0900

    connection: window-agg-couting

--- spark_connection/schema/coin_cal_query.py
+++ spark_connection/schema/coin_cal_query.py
@@ -10,61 +10,78 @@ from schema.data_constructure import (
 )
 
 
+def field_generator(field: Column) -> list[DataFrame]:
+    fields = [
+        "opening_price",
+        "trade_price",
+        "max_price",
+        "min_price",
+        "prev_closing_price",
+        "acc_trade_volume_24h",
+    ]
+    return [col(f"{field}.{market}") for market in fields]
+
+
+def column_generator(data: DataFrame, markets: list[str]) -> DataFrame:
+    for market in markets:
+        data = data.withColumn(market, *field_generator(market))
+    return data
+
+
 class SparkCoinAverageQueryOrganization:
 
     def __init__(self, kafka_data: DataFrame) -> None:
         self.kafka_cast_string = kafka_data.selectExpr("CAST(value AS STRING)")
-        self.markets = ["upbit","bithumb","coinone","korbit","gopax"]
-        self.fields = ["opening_price", "trade_price", "max_price", "min_price", "prev_closing_price", "acc_trade_volume_24h"]
+        self.markets = ["upbit", "bithumb", "coinone", "korbit", "gopax"]
 
-    def get_avg_field(self, field):
-        return F.round(
-             (col(f"crypto.upbit.data.{field}").cast("double") + 
-             col(f"crypto.bithumb.data.{field}").cast("double") +
-             col(f"crypto.coinone.data.{field}").cast("double") +
-             col(f"crypto.korbit.data.{field}").cast("double") +
-             col(f"crypto.gopax.data.{field}").cast("double")) / 5,3
-        ).alias(field)
-   
-    def coin_preprocessing(self) -> DataFrame:
+    # fmt: off
+    def coin_main_columns(self) -> DataFrame:
         return (
-            self.kafka_cast_string
-            .select(F.from_json("value", schema=final_schema).alias("crypto"))
+            self.kafka_cast_string.select(
+                F.from_json("value", schema=final_schema).alias("crypto")
+            )
             .select(
-                #F.to_timestamp(F.from_unixtime(earlist_timestamp)).alias("timestamp"),
-                F.to_timestamp(F.from_unixtime(
-                    F.least(
-                        col("crypto.upbit.time"),
-                        col("crypto.bithumb.time"),
-                        col("crypto.coinone.time"),
-                        col("crypto.korbit.time"),
-                        col("crypto.gopax.time")
+                F.to_timestamp(
+                    F.from_unixtime(
+                        F.least(
+                            col("crypto.upbit.time"),
+                            col("crypto.bithumb.time"),
+                            col("crypto.coinone.time"),
+                            col("crypto.korbit.time"),
+                            col("crypto.gopax.time"),
+                        )
                     )
-                )).alias("timestamp"),
-                *[col(f"crypto.{market}.time").alias(f"{market}_time") for market in self.markets],
-                *[self.get_avg_field(field) for field in self.fields]
-                #F.current_timestamp().alias("processed_time")
-            ).withWatermark(
-                "timestamp", "30 seconds"
-            ).groupby(
+                ).alias("timestamp"),
+                *[col(f"crypto.{market}.data").alias(f"{market}") for market in self.markets],
+            )
+        )
+
+    def coin_colum_window(self):
+        columns_selection = self.coin_main_columns()
+        data: DataFrame = column_generator(columns_selection, self.markets)
+        return (
+            data
+            .groupby(
                 F.window(col("timestamp"), "1 second", "1 second"),
                 col("timestamp"),
-                *[col(f"{market}_time") for market in self.markets]
-            ).agg(
-                #*[self.get_avg_field(field) for field in self.fields],
+                *[col(f"{market}_time") for market in self.markets],
+            )
+            .agg(
+                # *[self.get_avg_field(field) for field in self.fields],
                 *[F.count(col(field)).alias(f"{field}_count") for field in self.fields],
-                *[F.first(col(field)).alias(field) for field in self.fields]
-            ).select(
+                *[F.first(col(field)).alias(field) for field in self.fields],
+            )
+            .select(
                 *[col(f"{market}_time") for market in self.markets],
                 F.current_timestamp().alias("processed_time"),
                 col("timestamp"),
                 *[col(field) for field in self.fields],
                 *[col(f"{field}_count") for field in self.fields],
                 col("window.start").alias("window_start"),
-                col("window.end").alias("window_end")
+                col("window.end").alias("window_end"),
             )
         )
-        
+
     def socket_preprocessing(self) -> DataFrame:
         """웹소켓 처리"""
         return (
